d6e918f7afa51e8519dddb09f676b8c52b52f4bc
Ryuga <vaishnav2353@gmail.com>
2026-02-08 00:44:34 +0530

    fixes

--- core/settings.py
+++ core/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 PAYPAL_SECRET = config("PAYPAL_SECRET")
